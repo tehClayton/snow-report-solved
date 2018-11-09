bffea72cd2af9883e62821578e7878a00c7b3e42
Clayton Noyes <clayton.noyes@gmail.com>
2018-11-08 18:22:16 -0700

    Add splinter & scrape table

--- scraper.py
+++ scraper.py
@@ -1,10 +1,25 @@
-import bs4 as bs
-import requests
+from bs4 import BeautifulSoup as bs
+from splinter import Browser
+import urllib.request
+import time
+
+
+def init_browser():
+    # @NOTE: Replace the path with your actual path to the chromedriver
+    executable_path = {"executable_path": "/usr/local/bin/chromedriver"}
+    return Browser("chrome", **executable_path, headless=False)
 
 url = 'https://www.onthesnow.com/epic-pass/skireport.html'
 
-html = requests.get(url).text
+browser = init_browser()
+browser.visit(url)
+time.sleep(2)
 
-table = html.find('table', class_='resortList')
+
+soup = bs(browser.html, 'html.parser')
+
+table = soup.find_all('table')
+
+browser.quit()
 
 print(table)
